ad22d1d43dc548233264499c0b03b7348cc4607f
armando.vasconcelos <armando.vasconcelos@mv.com.br>
2026-09-01 11:35:35 -0300

    Projetos do curso - Java WEB

---
+++ java/Projetos de Cursos/tabelafipe/src/main/java/br/com/alura/tabelafipe/model/DadosVeiculos.java
@@ -0,0 +1,16 @@
+package br.com.alura.tabelafipe.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosVeiculo(@JsonAlias("TipoVeiculo") Integer TipoVeiculo,
+                           @JsonAlias("Valor") String Valor,
+                           @JsonAlias("Marca") String Marca,
+                           @JsonAlias("Modelo") String Modelo,
+                           @JsonAlias("AnoModelo") String AnoModelo,
+                           @JsonAlias("Combustivel") String Combustivel,
+                           @JsonAlias("CodigoFipe") String CodigoFipe,
+                           @JsonAlias("MesReferencia") String MesReferencia,
+                           @JsonAlias("SiglaCombustivel") String SiglaCombustivel) {
+}
